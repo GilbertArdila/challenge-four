054434a5cb82d0a258a7348dbf8a1c2d5c876fe4
Gilbert Ardila <gilbertferney@gmail.com>
2022-04-05 13:34:38 -0500

    header realizado

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+
+const buscador=document.getElementById("header-searcher__logo");
+const inputBuscador=document.getElementById("header-searcher__input");
+const estadoInput=inputBuscador.style;
+console.log(estadoInput.display)
+
+
+buscador.addEventListener("click", function(){
+    if(estadoInput.display==="" || estadoInput.display==="none" ){
+        inputBuscador.style.display="block"
+    }else{
+        inputBuscador.style.display="none" 
+    }
+});
