7c686e64e08bf1e886b3a01780d1d442ef309b01
Gilbert Ardila <gilbertferney@gmail.com>
2022-04-09 10:50:34 -0500

    pusimos las reglas necesarias para las diferentes vistas

--- scripts/index.js
+++ scripts/index.js
@@ -4,11 +4,13 @@ const inputBuscador=document.getElementById("header-searcher__input");
 const estadoInput=inputBuscador.style;
 console.log(estadoInput.display)
 
+if(screen.width< 768){
+    buscador.addEventListener("click", function(){
+        if(estadoInput.display==="" || estadoInput.display==="none" ){
+            inputBuscador.style.display="block"
+        }else{
+            inputBuscador.style.display="none" 
+        }
+    });
+};
 
-buscador.addEventListener("click", function(){
-    if(estadoInput.display==="" || estadoInput.display==="none" ){
-        inputBuscador.style.display="block"
-    }else{
-        inputBuscador.style.display="none" 
-    }
-});
