985f35d9269443dc47b1d88a0073e1ba64fd56b1
Gilbert Ardila <gilbertferney@gmail.com>
2022-04-18 14:17:26 -0500

    se han finalizado las verificaciones de los campos requeridos para poder crear los productos, CREATE o POST creado con exitogit status

--- components/createProduct.js
+++ components/createProduct.js
@@ -17,35 +17,79 @@ formulario.addEventListener("submit",(evento)=>{
     const descripcion=document.querySelector("[data-descripcion]").value;
     let picture;
     //verificamos el contenido de los campos para cargar la foto
-    picture=
-     btoa(verificarFoto(dragFile,selectedFile,picture));
-   
-    //mandamos a llamar el método para crear el producto
-    clientServices.crearProducto(picture,nombre,precio,clase,descripcion).then(respuesta=>{
-        alert("El producto se ha creado satisfactoriamente");
-    }).catch(error=>alert("Se ha producido un error"+error));
-   
-   
+    picture =btoa(verificarFoto(dragFile, selectedFile, picture))
+      console.log( picture );
+    if (verificarCampos(nombre, precio, clase) === true &&
+    picture!="") {
+        //mandamos a llamar el método para crear el producto
+        clientServices.crearProducto(picture, nombre, precio, clase, descripcion).then(respuesta => {
+            alert("El producto se ha creado satisfactoriamente");
+        }).catch(error => alert("Se ha producido un error" + error));
+    }
 }
 );
 
+const verificarCampos=(nombre,precio)=>{
+    let verificar=true;
+       if(nombre===""){
+          let mensaje=document.getElementById("error-nombre");
+           mensaje.innerText="Por favor rellene el campo con el nombre del producto"
+           verificar= false;
+       }else{
+        let mensaje=document.getElementById("error-nombre");
+        mensaje.innerText=""
+        
+       }
+       if(precio===""){
+        let mensaje=document.getElementById("error-precio");
+         mensaje.innerText="El campo precio no puede estar vacío"
+         verificar= false;
+        }else{
+            let mensaje=document.getElementById("error-precio");
+            mensaje.innerText="";
+            
+   
+        }
+     if(document.querySelector("[data-descripcion]").value===""){
+        let mensaje=document.getElementById("error-descripcion");
+        mensaje.innerText="Debe poner una descripción al producto";
+        verificar= false;
+
+        
+     }else{
+        let mensaje=document.getElementById("error-descripcion");
+        mensaje.innerText="";
+        
+       
+
+     }
+     
+     return verificar;
+}
 
 const verificarFoto=(dragFile,selectedFile,picture)=>{
     if(selectedFile==="" && dragFile!=""){
         picture=dragFile;
+        let mensaje=document.getElementById("error-foto");
+        mensaje.innerText=""
         
     }
     if(selectedFile!="" && dragFile===""){
         picture=selectedFile
+        let mensaje=document.getElementById("error-foto");
+        mensaje.innerText=""
         
     }
     if(selectedFile==="" && dragFile===""){
-       alert("Por favor ingrese la fotografia en uno de los dos campos habilitados");
-       picture=""
+        let mensaje=document.getElementById("error-foto");
+        mensaje.innerText="Por favor ingrese la foto del producto"
+        
+        picture=""
       
    }
    if(selectedFile!="" && dragFile!=""){
-        alert("Por favor ingrese la fotografia en uno de los dos campos habilitados, no en los dos");
+    let mensaje=document.getElementById("error-foto");
+    mensaje.innerText="Por ingrese la foto en uno solo de los dos campos, no en los dos"
         //limpiamos los dos campos de la imagen
         const selectedFile=document.querySelector("[data-selected]").value="";
         document.querySelector("[data-drop]").value="";
